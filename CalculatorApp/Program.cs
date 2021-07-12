43947428637bb7904237b192957a87b9f3d0d5ea
Azmitrok <azmitrok@gmail.com>
2021-07-12 19:05:09 +0300

    Initial commit

--- CalculatorApp/Program.cs
+++ CalculatorApp/Program.cs
@@ -6,7 +6,33 @@ namespace CalculatorApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Input number1:");
+            var operand1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Input oepration:");
+            var operation = Console.ReadLine()[0];
+
+            Console.WriteLine("Input number2:");
+            var operand2 = double.Parse(Console.ReadLine());
+
+            double result;
+
+            switch (operation)
+            {
+                case '+':
+                    result = operand1 + operand2;
+                    break;
+
+                case '-':
+                    result = operand1 - operand2;
+                    break;
+
+                default:
+                    throw new ArgumentException($"Operation not supported: {operation}");
+            }
+
+            Console.WriteLine($"Result: {result}");
+            Console.ReadLine();
         }
     }
 }
